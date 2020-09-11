3de6eccd0b62d10f6d7e96cf71bbbb6d9085c2ef
mhsammak <musabbirhasansammak@gmail.com>
2020-09-11 23:44:37 +0800

    solves broken keyboard

---
+++ chapter2/bit_manipulation/uva10264.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int corners;
+    while (scanf("%d", &corners)) {
+        
+    }
+    
+}
---
+++ chapter2/linked_list/uva11988.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<char> lists;
+string text;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (cin >> text) {
+        lists.clear();
+
+        list<char>::iterator it = lists.begin();
+        for (char c: text) {
+            if (c == '[') {
+                it = lists.begin();
+            } else if (c == ']') {
+                it = lists.end();
+            } else {
+                lists.insert(it, c);
+            }
+        }
+
+        for (auto it = lists.begin(); it != lists.end(); ++it {
+            cout << *it;
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
