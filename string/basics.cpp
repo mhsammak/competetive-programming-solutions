0c4becda205d961aefe2ef019e9b343a8544eb01
mhsammak <musabbirhasansammak@gmail.com>
2021-01-07 23:45:46 +0600

    string basics - 3

--- string/basics.cpp
+++ string/basics.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
 using namespace std;
 
@@ -31,5 +32,23 @@ int main() {
     while ((idx = T.find(P, start)) != -1) {
         pos.push_back(idx);
         start = idx;
+    }
+
+    const char *cstr = T.data();
+    
+    string vwl = "aeiou";
+    string cnt = "bcdfghjklmnpqrstvwxyz";
+    int digits = 0;
+    int vowels = 0;
+    int consnents = 0;
+
+    for (int i = 0; i < T.size(); i++) {
+        if (isdigit(T.at(i))) {
+            digits++;
+        } else if (find(vwl, T.at(i)) != -1) {
+            vowels++;
+        } else if (find(cnt, T.at(i)) != -1) {
+            consnents++;
+        }
     }
 }
